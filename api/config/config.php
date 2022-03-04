98d878c90f262be11c3f33c1d626e231c2a6a3da
eintyp <eintyp@duck.com>
2022-03-04 13:51:05 +0100

    new config format

---
+++ api/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+# set the environment to DEV if you want debug output
+#define('ENV', 'DEV');
+define('ENV', 'PROD');
+
+# add your database config here
+define('DB_NAME', 'my_database');
+define('DB_USER', 'my_user');
+define('DB_PASSWORD', 'my_password');
+define('DB_HOST', 'localhost');
+
+# if you want e-mail, uncomment these lines and add your e-mail config
+#define('MAIL_ADDRESS', 'root@localhost');
+#define('MAIL_PASSWORD', 'my_password');
+#define('MAIL_SMTP', 'smtp.myhost.tld');
+#define('MAIL_PORT', 587);
