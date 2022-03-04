d32c49c93f3b69335ed256fbcefe13716dccf82c
eintyp <eintyp@duck.com>
2022-03-04 18:26:30 +0100

    add http response codes to api

--- api/lib/Model.php
+++ api/lib/Model.php
@@ -7,6 +7,12 @@ abstract class Model
 
 	protected function return($data)
 	{
+		$this->response(200, $data);
+	}
+
+	protected function response($code, $data)
+	{
+		http_response_code($code);
 		if (is_array($data))
 		{
 			header('Content-Type: application/json');
