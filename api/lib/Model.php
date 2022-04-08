a0918ed87dfbc3d27c9d2ab3619603299c08e6cf
eintyp <eintyp@duck.com>
2022-04-08 20:19:29 +0200

    move method handling from Controller::autoload() to Model::call()

--- api/lib/Model.php
+++ api/lib/Model.php
@@ -13,6 +13,55 @@ abstract class Model
 
 
 
+	public function call($identifier, $attribute)
+	{
+		switch ($_SERVER['REQUEST_METHOD'])
+		{
+			case 'GET':
+				// GET /items/ [returns list with all items]
+				// this can be filtered using GET URL parameters,
+				// such as GET /items/?color=red [returns list with all red items]
+				if (!isset($identifier)) return $this->getAll($_SERVER['QUERY_STRING']);
+				// GET /items/42 [returns item #42]
+				if (!isset($attribute)) return $this->getSingle($identifier);
+				// GET /items/42/color [returns the color of item #42]
+				return $this->getAttribute($identifier, $attribute);
+			case 'POST':
+				// all post requests include a request body with some JSON data
+				$body = json_decode(file_get_contents('php://input'), true);
+				// POST /items/ [creates new item from request body data, returns id of the new element]
+				// for AUTO_INCREMENT only, if your model doesn't use A_I, use PUT instead
+				if (!isset($identifier)) return $this->createSingle($body);
+				// POST /items/42 [updates (not replaces) the trageted item #42 with data from the request body,
+				// keeps old values if they aren't mentioned in the request]
+				if (!isset($attribute)) return $this->updateSingle($identifier, $body);
+				// we don't allow using POST to set individual attributes,
+				// use POST /items/42 and wrap the atribute in a JSON object instead
+				return $this->api_response('Too Many Arguments', 400);
+			case 'PUT':
+				// like POST, all PUT requests include a request body
+				$body = json_decode(file_get_contents('php://input'), true);
+				// PUT /items/42 [replaces (not updates) the targeted item #42 with the request body,
+				// values not included in the body default to whatever the default attribute value is (null, empty string, etc)]
+				if (isset($identifier) && !isset($attribute)) return $this->replaceSingle($identifier, $body);
+				// we don't allow setting individual attributes (use POST instead),
+				// or replacing entire lists (if you really need this, you'll have to implement it yourself)
+				return $this->api_response('Bad Request', 400);
+			case 'DELETE':
+				// DELETE /items/42 [deletes item #42]
+				if (isset($identifier) && !isset($attribute)) return $this->deleteSingle($identifier);
+				// we don't allow deleting of attributes (not really possible, use POST to reset them),
+				// or deleting the entire list (obviously a bad idea, again, if you absolutely can't live without this, implement it yourself)
+				return $this->api_response('Unable To Delete Target Resource', 400);
+			default:
+				// other request methods are not available right now
+				// PATCH is implemented by POST, since that seems to be the general convention,
+				// HEAD and OPTIONS might be implemented later on, but those are only nice to have features (i know REST says otherwise)
+				return $this->api_response('Request Method Is Not Implemented By The Target Resource', 405);
+		}
+	}
+
+
 	public function getAll($filter)
 	{
 		$filter = explode('&', $filter);
@@ -124,5 +173,14 @@ abstract class Model
 		header('Content-Type: application/json');
 		echo json_encode($data);
 	}
+
+	/*
+	protected function text_response($text, $code = 200)
+	{
+		http_response_code($code);
+		header('Content-Type: text/plain');
+		echo json_encode($text);
+	}
+	*/
 
 }
